55237fb7c1f3059fca69eb4eb6291f2709289226
Robin Fleury <robin.fleury@oaknorth.ai>
2020-03-29 17:34:18 +0100

    Add Frontend

---
+++ frontend/src/components/covidtag.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function CovidTag(props) {
+  return "test";
+}
---
+++ frontend/src/components/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
